7bc7315218b559eba811bb15318c5d453e43470d
daveFrizky <frizkydavid@gmail.com>
2021-11-25 14:36:31 +0700

    update

---
+++ Sesi8/Sesi8/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Sesi8.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Sesi8/Sesi8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sesi8.WebApiApplication" Language="C#" %>
