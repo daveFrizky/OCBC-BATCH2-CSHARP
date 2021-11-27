b6dca654e6e63ee1a87ed12fef8e214b22b9c342
daveFrizky <frizkydavid@gmail.com>
2021-11-27 15:07:57 +0700

    update

--- Assignment3/MoviesAPI/Controllers/MoviesController.cs
+++ Assignment3/MoviesAPI/Controllers/MoviesController.cs
@@ -24,11 +24,35 @@ namespace MoviesAPI.Controllers
             return _context.GetAllMovies();
         }
 
+        [HttpPost(Name = "Add Movies")]
+        public ActionResult<string> AddMovies(MoviesItem item)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(MoviesContext)) as MoviesContext;
+            return _context.AddMovies(item);
+
+        }
+
         [HttpGet("{id}", Name = "Get Movies By Id")]
         public ActionResult<IEnumerable<MoviesItem>> GetMoviesItem(String id)
         {
             _context = HttpContext.RequestServices.GetService(typeof(MoviesContext)) as MoviesContext;
             return _context.GetMoviesById(id);
+
+        }
+
+        [HttpPut("{id}",Name = "Update Movies By Id")]
+        public ActionResult<string> UpdateMoviesById(string id,MoviesItem item)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(MoviesContext)) as MoviesContext;
+            return _context.UpdateById(id,item);
+
+        }
+
+        [HttpDelete("{id}", Name = "Delete Movies By Id")]
+        public ActionResult<string> DeleteMoviesById(string id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(MoviesContext)) as MoviesContext;
+            return _context.DeleteById(id);
 
         }
     }
--- Assignment3/MoviesAPI/Models/MoviesContext.cs
+++ Assignment3/MoviesAPI/Models/MoviesContext.cs
@@ -1,4 +1,6 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 
 namespace MoviesAPI.Models
@@ -43,6 +45,31 @@ namespace MoviesAPI.Models
             return list;
         }
 
+        public JsonResult AddMovies(MoviesItem item)
+        {
+            string result;
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(
+                    $"INSERT INTO movies VALUES ({item.Id},'{item.Name}','{item.Genre}','{item.Duration}" +
+                    $"','{item.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss")}')", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader != null)
+                    {
+                        result = "Data Added!";
+                    }
+                    else
+                    {
+                        result = "Something Went Wrong";
+                    }
+                }
+            }
+            return new JsonResult(result);
+        }
+
         public List<MoviesItem> GetMoviesById(string id)
         {
             List<MoviesItem> list = new List<MoviesItem>();
@@ -68,6 +95,54 @@ namespace MoviesAPI.Models
                 }
             }
             return list;
+        }
+
+        public JsonResult UpdateById(string id,MoviesItem item)
+        {
+            string result;
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(
+                    $"UPDATE movies SET  Id={item.Id},Name='{item.Name}',Genre='{item.Genre}',Duration='{item.Duration}" +
+                    $"',ReleaseDate='{item.ReleaseDate.ToString("yyyy-MM-dd HH:mm:ss")}'  WHERE Id = @id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader != null)
+                    {
+                        result = "Data Updated!";
+                    }
+                    else
+                    {
+                        result = "Something Went Wrong";
+                    }
+                }
+            }
+            return new JsonResult(result);
+        }
+
+        public JsonResult DeleteById(string id)
+        {
+            string result;
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM movies WHERE Id = @id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader != null)
+                    {
+                        result = "Data Deleted!";
+                    }
+                    else
+                    {
+                        result = "Something Went Wrong!";
+                    }
+                }
+            }
+            return new JsonResult(result);
         }
     }
 }
