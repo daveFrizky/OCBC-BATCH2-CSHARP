355b8c2f081a4a0c96d815f90701f3e5f5461b0c
daveFrizky <frizkydavid@gmail.com>
2021-11-29 11:06:32 +0700

    update

---
+++ Sesi12/UnitTest_Mock/UnitTest_Mock/Models/ApiDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTest_Mock.Models;
+
+namespace UnitTest_Mock.Model
+{
+    public partial class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Sesi12/UnitTest_Mock/UnitTest_Mock/Services/EmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnitTest_Mock.Model;
+using UnitTest_Mock.Models;
+
+namespace UnitTest_Mock.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        #region Property
+        private readonly ApiDbContext _apiDbContext;
+        #endregion
+
+        #region Constructer
+        public EmployeeService(ApiDbContext apiDbContext)
+        {
+            _apiDbContext = apiDbContext;
+        }
+
+        public Task<Employee> GetEmployeeDetails(int EmpId)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        public async Task<string> GetEmployeeId(int EmpId)
+        {
+            var name = await _apiDbContext.Employees.Where(c=>c.Id == EmpId).Select(d=> d.Name).FirstOrDefaultAsync();
+            return name;
+        }
+    }
+}
